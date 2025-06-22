9855a51f4507d02dff683600e3d325ea0ba655fc
muodilo <murindahabiodilo@gmail.com>
2025-06-22 21:41:15 +0200

    refactor: make logo a link to homepage

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Logo = () => {
   return (
-    <div>
+    <Link href={'/'}>
         <Image width={100} height={50} className='w-36' src='/logo.svg' alt='logo'/>
-    </div>
+    </Link>
   )
 }
 
